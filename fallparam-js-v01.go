d41c8f5b593883072501c70b2a48f5f1b226a422
sorna0day <136428149+sorna0day@users.noreply.github.com>
2023-07-24 14:21:08 +0330

    Update fallparam-js-v01.go

--- fallparam-js-v01.go
+++ fallparam-js-v01.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
 	"regexp"
 	"runtime"
 )
@@ -15,83 +14,53 @@ const version = "1.0.0"
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: fallparam -d <website>")
+		fmt.Println("Usage: jsparam -d <website>")
 		return
 	}
 
 	switch os.Args[1] {
 	case "-h":
-		fmt.Println("Usage: fallparam -d <website>")
+		fmt.Println("Usage: jsparam -d <website>")
 		fmt.Println("-h\t\tShow this help message")
 		fmt.Println("-v\t\tShow version")
-		fmt.Println("update-\t\tUpdate to the latest version")
-		fmt.Println("-r\t\tRemove fallparam from the server")
-	case "-v":
-		fmt.Println("fallparam version", version)
-	case "update-":
-		fmt.Println("Updating fallparam...")
-		cmd := exec.Command("git", "pull")
-		cmd.Dir = "/root/go/bin/fallparam "
-		err := cmd.Run()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("fallparam updated successfully")
 	case "-r":
-		fmt.Println("Removing fallparam...")
-		cmd := exec.Command("rm", "-rf", "/root/go/bin/fallparam ")
-		err := cmd.Run()
+		deleteTool()
+	case "-up":
+		updateTool()
+	default:
+		fmt.Println("Invalid command")
+	}
+}
+
+func deleteTool() {
+	// Get the operating system
+	os := runtime.GOOS
+
+	// Delete the tool based on the operating system
+	switch os {
+	case "windows":
+		err := os.Remove("C:\\path\\to\\jsparam.exe")
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println("fallparam removed successfully")
-	case "-d":
-		if len(os.Args) < 3 {
-			fmt.Println("Usage: fallparam -d <website>")
-			return
-		}
-
-		website := os.Args[2]
-
-		response, err := http.Get(website)
+	case "linux":
+		err := os.Remove("/usr/local/bin/jsparam")
 		if err != nil {
 			log.Fatal(err)
 		}
-		defer response.Body.Close()
-
-		body, err := ioutil.ReadAll(response.Body)
+	case "darwin":
+		err := os.Remove("/usr/local/bin/jsparam")
 		if err != nil {
 			log.Fatal(err)
-		}
-
-		varNameRegex := regexp.MustCompile(`\bvar\s+(\w+)\s+=`)
-		variables := varNameRegex.FindAllStringSubmatch(string(body), -1)
-
-		jsonRegex := regexp.MustCompile(`\bJSON\.parse\((.*?)\)`)
-		jsonObjects := jsonRegex.FindAllStringSubmatch(string(body), -1)
-
-		fmt.Println("JavaScript variables names:")
-		for _, match := range variables {
-			fmt.Println(match[1])
-		}
-
-		fmt.Println("JSON object in JavaScript files:")
-		for _, match := range jsonObjects {
-			fmt.Println(match[1])
 		}
 	default:
-		fmt.Println("Usage: fallparam -d <website>")
+		fmt.Println("Unsupported operating system")
 	}
+}
 
-	if runtime.GOOS == "linux" {
-		fmt.Println("This is a Linux system")
-	} else if runtime.GOOS == "windows" {
-		fmt.Println("This is a Windows system")
-	} else if runtime.GOOS == "darwin" {
-		fmt.Println("This is a macOS system")
-	}
-
-	if len(os.Args) > 1 && os.Args[1] == "fallparam" {
-		fmt.Println("fallparam")
-	}
+func updateTool() {
+	// Download and install the latest version of the tool from GitHub
+	fmt.Println("Updating jsparam...")
+	// Add code to download and install the latest version of the tool from GitHub
+	fmt.Println("jsparam updated successfully!")
 }
